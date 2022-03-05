6acb696658154c92eefae61b49edd75666343080
Сергей <tyapkin2002@mail.ru>
2022-03-05 20:06:08 +0300

    Настроил сервер

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'source')));
+app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 app.listen(PORT, 'localhost', () => {
     console.log("Server listening on port: " + PORT);
 });
