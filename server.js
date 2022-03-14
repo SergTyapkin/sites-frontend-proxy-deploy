c9cd6530b20990169e06f2667565da8f3480d20e
Сергей <tyapkin2002@mail.ru>
2022-03-14 17:58:47 +0300

    -

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const proxy = require("express-http-proxy");
 const app = express();
 
-STATIC_DIR = path.resolve(__dirname)
-INDEX_PATH = path.resolve(__dirname, 'index.html')
+const
+    STATIC_DIR = path.resolve(__dirname);
+    INDEX_PATH = path.resolve(__dirname, 'index.html');
+    API_URL = 'http://squest-api.herokuapp.com/api';
+
+app.use('/api', proxy(API_URL));
 
 app.use(express.static(STATIC_DIR));
 app.get('/*', (req, res) => {
